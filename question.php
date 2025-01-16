48a01cee18e8574b40578eced087ed3b5027aed4
Lucas <lucas.devers-dore@etu.univ-orleans.fr>
2025-01-16 19:19:34 +0100

    classe question

---
+++ question.php
@@ -0,0 +1,25 @@
+<?php
+
+class Question {
+    private string $texte;
+    private array $choix;
+    private string $bonneRep;
+
+    public function __construct(string $texte, array $choix, string $bonneRep) {
+        $this->texte = $texte;
+        $this->choix = $choix;
+        $this->bonneRep = $bonneRep;
+    }
+
+    public function getTexte(): string {
+        return $this->texte;
+    }
+
+    public function getchoix(): array {
+        return $this->choix;
+    }
+
+    public function estCorrect(string $bonneRep): bool {
+        return $this->bonneRep === $bonneRep;
+    }
+}
